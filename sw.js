1a67f3f179c9fe3a6ab86926e08e122d6c209282
niutongtong <niu_yitong@163.com>
2019-01-14 15:08:46 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,10 +1,8 @@
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.1.0/workbox-sw.js");
+
 var cacheStorageKey = 'minimal-pwa-1'
 var cacheList=[
-  '/',
-  'index.html',
-  'main.css',
-  'youhun.jpg'
+  '/0114-PWA-project/index.html',
+  '/0114-PWA-project/main.css'
 ]
 self.addEventListener('install',e =>{
   e.waitUntil(
@@ -13,35 +11,26 @@ self.addEventListener('install',e =>{
     .then(() => self.skipWaiting())
   )
 })
-self.addEventListener('fetch',function(e){
-  e.respondWith(
-    caches.match(e.request).then(function(response){
-      if(response != null){
-        return response
-      }
-      return fetch(e.request.url)
-    })
-  )
-})
-self.addEventListener('activate',function(e){
-  e.waitUntil(
-    //获取所有cache名称
-    caches.keys().then(cacheNames => {
-      return Promise.all(
-        // 获取所有不同于当前版本名称cache下的内容
-        cacheNames.filter(cacheNames => {
-          return cacheNames !== cacheStorageKey
-        }).map(cacheNames => {
-          return caches.delete(cacheNames)
+self.addEventListener('fetch', function (event) {
+    event.respondWith(
+        caches.match(event.request).then(function(response){
+            if(response){
+                return response;
+            }
+            var requestToCache = event.request.clone();
+            return fetch(requestToCache).then(
+              function(response){
+                if(!response || response.status !== 200){
+                  return response;
+                }
+                var responseToCache = response.clone();
+                caches.open(cacheStorageKey)
+                  .then(function(cache){
+                    cache.put(requestToCache, responseToCache);
+                  });
+                return response;
+              }
+            );
         })
-      )
-    }).then(() => {
-      return self.clients.claim()
-    })
-  )
-})
-
-作者：小申同学
-链接：https://www.jianshu.com/p/fad8aa9e277f
-來源：简书
-简书著作权归作者所有，任何形式的转载都请联系作者获得授权并注明出处。
+    );
+});
